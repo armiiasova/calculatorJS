40065ab718523f83a5ce9d44e3e44bba6fa7bc8b
Mairam Armiiasova <123258732+armiiasova@users.noreply.github.com>
2023-02-25 20:49:07 +0100

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,6 @@
+//Write a function called squareNumber that will take one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+function squareNumber(num)
+    let squaredNumber = num*number
+    console.log("The result of square the number" + num + "is" + squaredNumber) 
+    return squaredNumber
+  
